31f9ec496a62023fd83474d8d4267d0f89a4e9b6
Carlos Pavanetti <carlospavanetti.silvaprado@gmail.com>
2019-03-06 17:37:12 -0300

    Add `{HOME,END}_KEY` as detected keys

--- kilo.c
+++ kilo.c
@@ -19,6 +19,8 @@ enum editorKey {
   ARROW_RIGHT,
   ARROW_UP,
   ARROW_DOWN,
+  HOME_KEY,
+  END_KEY,
   PAGE_UP,
   PAGE_DOWN,
 };
@@ -81,6 +83,8 @@ int editorReadKey() {
         if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
         if (seq[2] == '~') {
           switch (seq[1]) {
+            case '1': return HOME_KEY;
+            case '4': return END_KEY;
             case '5': return PAGE_UP;
             case '6': return PAGE_DOWN;
           }
@@ -91,6 +95,8 @@ int editorReadKey() {
           case 'B': return ARROW_DOWN; 
           case 'C': return ARROW_RIGHT; 
           case 'D': return ARROW_LEFT; 
+          case 'H': return HOME_KEY; 
+          case 'F': return END_KEY; 
         }
       }
     }
